5bba6760fd9868a68ab4acf3b1807f6bc9bc08e0
kiya69 <kiya69@gmail.com>
2019-10-03 20:20:04 +0200

    adding modal

--- src/components/TournamentItem/TournamentItem.js
+++ src/components/TournamentItem/TournamentItem.js
@@ -1,20 +1,18 @@
 import React from 'react';
+import classes from './TournamentItem.module.css'
 
 const tournamentItem = (props) => {
     return (
-        <div>
-            <table>
-                <tr>
-                    <th>{props.id}</th>
-                    <th>{props.name}</th>
-                    <th>{props.country}</th>
-                    <th>{props.city}</th>
-                    <th>{props.date_start}</th>
-                    <th>{props.date_end}</th>
-                    <th>{props.series.name}</th>
+                <tr className={classes.TournamentItem} onClick={props.clicked}>
+                    <td>{props.tournament.id}</td>
+                    <td>{props.tournament.name}</td>
+                    <td>{props.tournament.country}</td>
+                    <td>{props.tournament.city}</td>
+                    <td>{props.tournament.date_start}</td>
+                    <td>{props.tournament.date_end}</td>
+                    <td>{props.tournament.series.name}</td>
                 </tr>
-            </table>
-        </div>
+          
     )
 }
 
--- src/containers/Tournaments/Tournaments.js
+++ src/containers/Tournaments/Tournaments.js
@@ -1,50 +1,110 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import TournamentItem from '../../components/TournamentItem/TournamentItem';
+import classes from './Tournaments.module.css';
+import Modal from '../../components/UI/Modal/Modal';
+
+const TOURNAMENTS_HEADERS = {
+    id: 'ID',
+    name: 'NAME',
+    country: 'COUNTRY',
+    city: 'CITY',
+    date_start: 'START DATE',
+    date_end: 'END DATE',
+    series: {
+        id: 'SERIES ID',
+        name: ' SERIES NAME',
+        date_start: 'START DATE',
+        date_end: 'END DATE'
+    }
+}
 
 class Tournaments extends Component {
 
     state = {
         tournaments: [],
-        error: false
+        error: false,
+        showTournamentDetail: false,
+        currentTournamentId: null
     }
 
     componentDidMount() {
         axios.get('https://gist.githubusercontent.com/idrinkritalin/fce0f5b884ffd813850ffb6919fe6bf7/raw/51007b611a52c427e34dbe9d40e91490e17c5248/tournaments.json')
-        .then(response => {
-            console.log(response.data);
-
-            this.setState({
-                tournaments: [...response.data]
+            .then(response => {
+                this.setState({
+                    tournaments: [...response.data]
+                })
             })
-        })
-        .catch(error => {
-            console.log(error);
-            this.setState({error: true})
-        });
+            .catch(error => {
+                console.log(error);
+                this.setState({ error: true })
+            });
     }
 
-    render(){
-        let tournaments = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-        if(!this.state.error) {
-            tournaments = this.state.tournaments.map(tournament => {
-                return (
-                    <TournamentItem 
-                        key={tournament.id}
-                        id={tournament.id}
-                        name={tournament.name}
-                        country={tournament.country}
-                        city={tournament.city}
-                        date_start={tournament.date_start}
-                        date_end={tournament.date_end}
-                        series={tournament.series} />
-                );
-            });
-        }
-        return <div>
-                    <h1>Tournaments</h1>
+    tournamentClickHandler = (id) => {
+        console.log(id);
+        //TODO: maybe use currentTournamentId only?
+        this.setState({
+            showTournamentDetail: true,
+            currentTournamentId: id
+        })
+    }
+
+    hideModalHandler =() => {
+
+        this.setState({
+            showTournamentDetail: false,
+            currentTournamentId: null
+        })
+    }
+
+    render() {
+        let tournamentTable = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
+
+        if (!this.state.error) {
+            const tournaments = this.state.tournaments.map(tournament =>
+                <TournamentItem
+                    key={tournament.id}
+                    tournament={tournament}
+                    clicked={() => this.tournamentClickHandler(tournament.id)}
+                    />
+            )
+            tournamentTable =  <table className={classes.TournamentTable}>
+                <thead>
+                    <tr key="tournaments_headers">
+                        <th>{TOURNAMENTS_HEADERS.id}</th>
+                        <th>{TOURNAMENTS_HEADERS.name}</th>
+                        <th>{TOURNAMENTS_HEADERS.country}</th>
+                        <th>{TOURNAMENTS_HEADERS.city}</th>
+                        <th>{TOURNAMENTS_HEADERS.date_start}</th>
+                        <th>{TOURNAMENTS_HEADERS.date_end}</th>
+                        <th>{TOURNAMENTS_HEADERS.series.name}</th>
+                    </tr>
+                </thead>
+                <tbody>
                     {tournaments}
-                </div>
+                </tbody>                    
+            </table>
+            
+        }
+        let tournamentSummary = null;
+        if(this.state.currentTournamentId){
+            console.log(this.state.tournaments[this.state.currentTournamentId])
+            tournamentSummary = <div>
+                {this.state.tournaments[this.state.currentTournamentId]}
+            </div>
+        }
+        return (
+            <div>
+
+                <Modal show={this.state.showTournamentDetail} modalClosed={this.hideModalHandler}>
+                    {tournamentSummary}
+                </Modal>
+                <h1>Tournaments</h1>
+
+                {tournamentTable}
+            </div>
+        )
     }
 }
 
