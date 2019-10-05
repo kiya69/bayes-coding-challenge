27620234c94016f350877c76ee023e8fd4e3ff0d
kiya69 <kiya69@gmail.com>
2019-10-05 13:53:38 +0200

    Remove react-router-dom and some error handling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
 import Tournaments from './containers/Tournaments/Tournaments';
-import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
       <div className="App">
         <Tournaments />
       </div>
-    </BrowserRouter>
   );
 }
 
